a742535547a5bc42e8c0b44d24e3f8bf5769538e
Samuel Azcona <samuaz@gmail.com>
2021-10-16 02:55:04 -0300

    use fetch make available

--- cmake/util_grpc_proto.cmake
+++ cmake/util_grpc_proto.cmake
@@ -7,49 +7,56 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET OFF)
 set(THIRD_PARTY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party)
 
-MESSAGE("grpc_SOURCE_DIR:" ${grpc_SOURCE_DIR})
+# for some reason the 3 steps together dont work so i split it
 
+# create grpc_cpp_plugin build dir
 execute_process(
         COMMAND mkdir -p build
         WORKING_DIRECTORY ${grpc_SOURCE_DIR}
-        RESULT_VARIABLE grpc_plugin_cmake_result
-        OUTPUT_VARIABLE grpc_plugin_cmake_VARIABLE)
-MESSAGE(STATUS "grpc_plugin_build_CMD_ERROR:" ${grpc_plugin_cmake_result})
-MESSAGE(STATUS "grpc_plugin_build_CMD_OUTPUT:" ${grpc_plugin_cmake_VARIABLE})
+        RESULT_VARIABLE grpc_plugin_build_dir_creation_result
+        OUTPUT_VARIABLE grpc_plugin_build_dir_creation_result_output)
+MESSAGE(STATUS "grpc_plugin_build_dir_creation_error:" ${grpc_plugin_build_dir_creation_result})
+MESSAGE(STATUS "grpc_plugin_build_dir_creation_output:" ${grpc_plugin_build_dir_creation_result_output})
 
 
+# create grpc_cpp_plugin cmake configuration
 execute_process(
         COMMAND cmake ..
         WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build
         RESULT_VARIABLE grpc_plugin_cmake_result
-        OUTPUT_VARIABLE grpc_plugin_cmake_VARIABLE)
-MESSAGE(STATUS "grpc_plugin_build_CMD_ERROR:" ${grpc_plugin_cmake_result})
-MESSAGE(STATUS "grpc_plugin_build_CMD_OUTPUT:" ${grpc_plugin_cmake_VARIABLE})
+        OUTPUT_VARIABLE grpc_plugin_cmake_result_output)
+MESSAGE(STATUS "grpc_plugin_cmake_CMD_ERROR:" ${grpc_plugin_cmake_result})
+MESSAGE(STATUS "grpc_plugin_cmake_CMD_OUTPUT:" ${grpc_plugin_cmake_result_output})
 
+# create grpc_cpp_plugin build
 execute_process(
         COMMAND make grpc_cpp_plugin
         WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build
-        RESULT_VARIABLE grpc_plugin_cmake_result
-        OUTPUT_VARIABLE grpc_plugin_cmake_VARIABLE)
-MESSAGE(STATUS "grpc_plugin_build_CMD_ERROR:" ${grpc_plugin_cmake_result})
-MESSAGE(STATUS "grpc_plugin_build_CMD_OUTPUT:" ${grpc_plugin_cmake_VARIABLE})
+        RESULT_VARIABLE grpc_plugin_make_result
+        OUTPUT_VARIABLE grpc_plugin_make_output)
+MESSAGE(STATUS "grpc_plugin_make_CMD_ERROR:" ${grpc_plugin_make_result})
+MESSAGE(STATUS "grpc_plugin_make_CMD_OUTPUT:" ${grpc_plugin_make_output})
 
+# create grpc_cpp_plugin copy to thirparty dir
 execute_process(
         COMMAND mkdir -p ${THIRD_PARTY_DIR}/grpc_plugin
         COMMAND cp ${grpc_SOURCE_DIR}/build/grpc_cpp_plugin ${THIRD_PARTY_DIR}/grpc_plugin
         COMMAND cp ${grpc_SOURCE_DIR}/build/libgrpc_plugin_support.a ${THIRD_PARTY_DIR}/grpc_plugin
         WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build
         RESULT_VARIABLE grpc_plugin_install_result
-        OUTPUT_VARIABLE grpc_plugin_OUTPUT_VARIABLE)
-MESSAGE(STATUS "grpc_plugin_CMD_ERROR:" ${grpc_plugin_install_result})
-MESSAGE(STATUS "grpc_plugin_CMD_OUTPUT:" ${grpc_plugin_OUTPUT_VARIABLE})
+        OUTPUT_VARIABLE grpc_plugin_install_out)
+MESSAGE(STATUS "grpc_plugin_install_ERROR:" ${grpc_plugin_install_result})
+MESSAGE(STATUS "grpc_plugin_install_OUTPUT:" ${grpc_plugin_install_out})
 set(grpc_plugin_SOURCE_DIR ${THIRD_PARTY_DIR}/grpc_plugin)
 
+
+# protobuf compiler 
 FetchContent_Declare(
         protobuf_compiler
         URL https://github.com/protocolbuffers/protobuf/releases/download/v3.18.0/protoc-3.18.0-${HOST_OS}-${HOST_ARCHITECTURE}.zip
         SOURCE_DIR ${THIRD_PARTY_DIR}/protobuf_compiler
 )
+
 set(protobuf_BUILD_TESTS OFF)
 
 # FetchContent_Declare(
