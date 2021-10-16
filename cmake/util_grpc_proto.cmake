5226635243de91b2947bdce0d64381b2c4e95cf7
Samuel Azcona <samuaz@gmail.com>
2021-10-16 02:14:08 -0300

    use fetch make available

--- cmake/util_grpc_proto.cmake
+++ cmake/util_grpc_proto.cmake
@@ -7,20 +7,21 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET OFF)
 set(THIRD_PARTY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party)
 
-# execute_process(
-#         COMMAND mkdir -p build && cd build && cmake .. &&  make 
-#         WORKING_DIRECTORY ${grpc_SOURCE_DIR}
-#         RESULT_VARIABLE mysql_cmake_result
-#         OUTPUT_VARIABLE mysql_cmake_VARIABLE)
-# MESSAGE(STATUS "MYSQL_cmake_CMD_ERROR:" ${mysql_cmake_result})
-# MESSAGE(STATUS "MYSQL_cmake_CMD_OUTPUT:" ${mysql_cmake_VARIABLE})
-# execute_process(
-#         COMMAND make install
-#         WORKING_DIRECTORY ${mysql_connector_cpp_SOURCE_DIR}
-#         RESULT_VARIABLE mysql_install_result
-#         OUTPUT_VARIABLE mysql_OUTPUT_VARIABLE)
-# MESSAGE(STATUS "MYSQL_INSTALL_CMD_ERROR:" ${mysql_install_result})
-# MESSAGE(STATUS "MYSQL_INSTALL_CMD_OUTPUT:" ${mysql_OUTPUT_VARIABLE})
+execute_process(
+        COMMAND mkdir -p build && cd build && cmake .. && make grpc_cpp_plugin
+        WORKING_DIRECTORY ${grpc_SOURCE_DIR}
+        RESULT_VARIABLE grpc_plugin_cmake_result
+        OUTPUT_VARIABLE grpc_plugin_cmake_VARIABLE)
+MESSAGE(STATUS "grpc_plugin_build_CMD_ERROR:" ${grpc_plugin_cmake_result})
+MESSAGE(STATUS "grpc_plugin_build_CMD_OUTPUT:" ${grpc_plugin_cmake_VARIABLE})
+execute_process(
+        COMMAND mkdir -p ${THIRD_PARTY_DIR}/grpc_plugin && cp ${grpc_SOURCE_DIR}/build/{grpc_cpp_plugin,libgrpc_plugin_support.a} ${THIRD_PARTY_DIR}/grpc_plugin
+        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build
+        RESULT_VARIABLE grpc_plugin_install_result
+        OUTPUT_VARIABLE grpc_plugin_OUTPUT_VARIABLE)
+MESSAGE(STATUS "grpc_plugin_CMD_ERROR:" ${grpc_plugin_install_result})
+MESSAGE(STATUS "grpc_plugin_CMD_OUTPUT:" ${grpc_plugin_OUTPUT_VARIABLE})
+set(grpc_plugin_SOURCE_DIR ${THIRD_PARTY_DIR}/grpc_plugin)
 
 FetchContent_Declare(
         protobuf_compiler
@@ -29,9 +30,9 @@ FetchContent_Declare(
 )
 set(protobuf_BUILD_TESTS OFF)
 
-FetchContent_Declare(
-        grpc_plugin
-        URL https://packages.grpc.io/archive/2021/09/253d7076fc19c7380b3f58b598eaca1b076bec74-e2a3a12f-5674-4798-9ea6-61228b2acbdc/protoc/grpc-protoc_${HOST_OS_2}_${HOST_ARCHITECTURE_2}-1.42.0-dev.tar.gz
-        SOURCE_DIR ${THIRD_PARTY_DIR}/grpc_plugin
-)
-FetchContent_MakeAvailable(protobuf_compiler grpc_plugin)
+# FetchContent_Declare(
+#         grpc_plugin
+#         URL https://packages.grpc.io/archive/2021/09/253d7076fc19c7380b3f58b598eaca1b076bec74-e2a3a12f-5674-4798-9ea6-61228b2acbdc/protoc/grpc-protoc_${HOST_OS_2}_${HOST_ARCHITECTURE_2}-1.42.0-dev.tar.gz
+#         SOURCE_DIR ${THIRD_PARTY_DIR}/grpc_plugin
+# )
+FetchContent_MakeAvailable(protobuf_compiler)
