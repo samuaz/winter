23a8a27f4cb128ca47c0db6f36f31a0f8b3b84c2
Samuel Azcona <samuaz@gmail.com>
2022-02-07 01:03:52 -0300

    use openssl fips

--- cmake/dep_openssl.cmake
+++ cmake/dep_openssl.cmake
@@ -12,7 +12,7 @@ set(THIRD_PARTY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party)
 FetchContent_Declare(
         openssl
         GIT_REPOSITORY https://github.com/openssl/openssl.git
-        GIT_TAG        OpenSSL_1_1_1-stable
+        GIT_TAG        OpenSSL-fips-2_0-stable
         GIT_PROGRESS   TRUE
         CONFIGURE_COMMAND config --prefix=${THIRD_PARTY_DIR}/openssl
         INSTALL_COMMAND make install_sw
@@ -55,7 +55,7 @@ endif ()
 ]]
 
 execute_process(
-        COMMAND ./config --prefix=${openssl_SOURCE_DIR}
+        COMMAND ./config no-asm --prefix=${openssl_SOURCE_DIR}
         WORKING_DIRECTORY ${openssl_SOURCE_DIR}
         RESULT_VARIABLE openssl_install_result
         OUTPUT_VARIABLE openssl_OUTPUT_VARIABLE)
