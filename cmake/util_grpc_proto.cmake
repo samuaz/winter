9a7853d1043e17593cbd21a1abf91c36e7620d5c
Samuel Azcona <samuaz@gmail.com>
2022-08-20 03:49:46 -0300

    add grpc plugin

--- cmake/util_grpc_proto.cmake
+++ cmake/util_grpc_proto.cmake
@@ -27,41 +27,41 @@ FetchContent_MakeAvailable(protobuf_compiler grpc_plugin)
 # for some reason the 3 steps together dont work so i split it
 
 # create grpc_cpp_plugin build dir
-execute_process(
-        COMMAND mkdir -p build
-        WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}
-        RESULT_VARIABLE grpc_plugin_build_dir_creation_result
-        OUTPUT_VARIABLE grpc_plugin_build_dir_creation_result_output)
-MESSAGE(STATUS "grpc_plugin_build_dir_creation_error:" ${grpc_plugin_build_dir_creation_result})
-MESSAGE(STATUS "grpc_plugin_build_dir_creation_output:" ${grpc_plugin_build_dir_creation_result_output})
+# execute_process(
+#         COMMAND mkdir -p build
+#         WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}
+#         RESULT_VARIABLE grpc_plugin_build_dir_creation_result
+#         OUTPUT_VARIABLE grpc_plugin_build_dir_creation_result_output)
+# MESSAGE(STATUS "grpc_plugin_build_dir_creation_error:" ${grpc_plugin_build_dir_creation_result})
+# MESSAGE(STATUS "grpc_plugin_build_dir_creation_output:" ${grpc_plugin_build_dir_creation_result_output})
 
 
-# create grpc_cpp_plugin cmake configuration
-execute_process(
-        COMMAND cmake ..
-        WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}/build
-        RESULT_VARIABLE grpc_plugin_cmake_result
-        OUTPUT_VARIABLE grpc_plugin_cmake_result_output)
-MESSAGE(STATUS "grpc_plugin_cmake_CMD_ERROR:" ${grpc_plugin_cmake_result})
-MESSAGE(STATUS "grpc_plugin_cmake_CMD_OUTPUT:" ${grpc_plugin_cmake_result_output})
+# # create grpc_cpp_plugin cmake configuration
+# execute_process(
+#         COMMAND cmake ..
+#         WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}/build
+#         RESULT_VARIABLE grpc_plugin_cmake_result
+#         OUTPUT_VARIABLE grpc_plugin_cmake_result_output)
+# MESSAGE(STATUS "grpc_plugin_cmake_CMD_ERROR:" ${grpc_plugin_cmake_result})
+# MESSAGE(STATUS "grpc_plugin_cmake_CMD_OUTPUT:" ${grpc_plugin_cmake_result_output})
 
-# create grpc_cpp_plugin build
-execute_process(
-        COMMAND make grpc_cpp_plugin
-        WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}/build
-        RESULT_VARIABLE grpc_plugin_make_result
-        OUTPUT_VARIABLE grpc_plugin_make_output)
-MESSAGE(STATUS "grpc_plugin_make_CMD_ERROR:" ${grpc_plugin_make_result})
-MESSAGE(STATUS "grpc_plugin_make_CMD_OUTPUT:" ${grpc_plugin_make_output})
+# # create grpc_cpp_plugin build
+# execute_process(
+#         COMMAND make grpc_cpp_plugin
+#         WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}/build
+#         RESULT_VARIABLE grpc_plugin_make_result
+#         OUTPUT_VARIABLE grpc_plugin_make_output)
+# MESSAGE(STATUS "grpc_plugin_make_CMD_ERROR:" ${grpc_plugin_make_result})
+# MESSAGE(STATUS "grpc_plugin_make_CMD_OUTPUT:" ${grpc_plugin_make_output})
 
-# create grpc_cpp_plugin copy to thirparty dir
-execute_process(
-        COMMAND mkdir -p ${THIRD_PARTY_DIR}/grpc_plugin
-        COMMAND cp ${grpc_plugin_SOURCE_DIR}/build/grpc_cpp_plugin ${THIRD_PARTY_DIR}/grpc_plugin
-        COMMAND cp ${grpc_plugin_SOURCE_DIR}/build/libgrpc_plugin_support.a ${THIRD_PARTY_DIR}/grpc_plugin
-        WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}/build
-        RESULT_VARIABLE grpc_plugin_install_result
-        OUTPUT_VARIABLE grpc_plugin_install_out)
-MESSAGE(STATUS "grpc_plugin_install_ERROR:" ${grpc_plugin_install_result})
-MESSAGE(STATUS "grpc_plugin_install_OUTPUT:" ${grpc_plugin_install_out})
+# # create grpc_cpp_plugin copy to thirparty dir
+# execute_process(
+#         COMMAND mkdir -p ${THIRD_PARTY_DIR}/grpc_plugin
+#         COMMAND cp ${grpc_plugin_SOURCE_DIR}/build/grpc_cpp_plugin ${THIRD_PARTY_DIR}/grpc_plugin
+#         COMMAND cp ${grpc_plugin_SOURCE_DIR}/build/libgrpc_plugin_support.a ${THIRD_PARTY_DIR}/grpc_plugin
+#         WORKING_DIRECTORY ${grpc_plugin_SOURCE_DIR}/build
+#         RESULT_VARIABLE grpc_plugin_install_result
+#         OUTPUT_VARIABLE grpc_plugin_install_out)
+# MESSAGE(STATUS "grpc_plugin_install_ERROR:" ${grpc_plugin_install_result})
+# MESSAGE(STATUS "grpc_plugin_install_OUTPUT:" ${grpc_plugin_install_out})
 set(grpc_plugin_SOURCE_DIR ${THIRD_PARTY_DIR}/grpc_plugin)
