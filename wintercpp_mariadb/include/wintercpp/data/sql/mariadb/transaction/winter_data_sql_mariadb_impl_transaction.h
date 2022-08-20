c7fe7133a0e9bb8d37e7e4326d3360d10b99e66a
Samuel Azcona <samuaz@gmail.com>
2022-08-20 05:09:41 -0300

    update transaction

--- wintercpp_mariadb/include/wintercpp/data/sql/mariadb/transaction/winter_data_sql_mariadb_impl_transaction.h
+++ wintercpp_mariadb/include/wintercpp/data/sql/mariadb/transaction/winter_data_sql_mariadb_impl_transaction.h
@@ -5,8 +5,9 @@
 #ifndef WINTERCPP_WINTER_DATA_SQL_MARIADB_IMPL_TRANSACTION_H
 #define WINTERCPP_WINTER_DATA_SQL_MARIADB_IMPL_TRANSACTION_H
 
-#include <wintercpp/data/sql/mariadb/connection/winter_data_sql_mariadb_impl_connection.h>
+#include <wintercpp/winter.h>
 #include <wintercpp/winter_mysql_core.h>
+#include <wintercpp/data/sql/mariadb/connection/winter_data_sql_mariadb_impl_connection.h>
 
 namespace winter::data::mariadb {
 
