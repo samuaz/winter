c28543578973eb12f5cd0e3d7d13346805086c43
samuaz <samuaz@gmail.com>
2022-09-21 04:35:34 -0300

    fix apple pthreads

--- cmake/util_grpc_proto.cmake
+++ cmake/util_grpc_proto.cmake
@@ -11,9 +11,11 @@ set(THIRD_PARTY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party)
 
 MESSAGE(STATUS "USING GRPC SOURCE DIR" ${grpc_SOURCE_DIR})
 
+set(BUILD_DIRECTORY build_grpc_plugin)
+
 # create grpc_cpp_plugin build dir
 execute_process(
-        COMMAND bash "-c" "mkdir build_grpc_plugin"
+        COMMAND bash "-c" "mkdir ${BUILD_DIRECTORY}"
         WORKING_DIRECTORY ${grpc_SOURCE_DIR}
         RESULT_VARIABLE grpc_plugin_build_dir_creation_result
         OUTPUT_VARIABLE grpc_plugin_build_dir_creation_result_output)
@@ -24,7 +26,7 @@ MESSAGE(STATUS "grpc_plugin_build_dir_creation_output:" ${grpc_plugin_build_dir_
 # create grpc_cpp_plugin cmake configuration
 execute_process(
         COMMAND bash "-c" "cmake .."
-        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build_grpc_plugin
+        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/${BUILD_DIRECTORY}
         RESULT_VARIABLE grpc_plugin_cmake_result
         OUTPUT_VARIABLE grpc_plugin_cmake_result_output)
 MESSAGE(STATUS "grpc_plugin_cmake_CMD_ERROR:" ${grpc_plugin_cmake_result})
@@ -33,7 +35,7 @@ MESSAGE(STATUS "grpc_plugin_cmake_CMD_OUTPUT:" ${grpc_plugin_cmake_result_output
 # create grpc_cpp_plugin build
 execute_process(
         COMMAND bash "-c" "make grpc_cpp_plugin"
-        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build_grpc_plugin
+        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/${BUILD_DIRECTORY}
         RESULT_VARIABLE grpc_plugin_make_result
         OUTPUT_VARIABLE grpc_plugin_make_output)
 MESSAGE(STATUS "grpc_plugin_make_CMD_ERROR:" ${grpc_plugin_make_result})
@@ -42,7 +44,7 @@ MESSAGE(STATUS "grpc_plugin_make_CMD_OUTPUT:" ${grpc_plugin_make_output})
 # create grpc_cpp_plugin copy to thirparty dir
 execute_process(
         COMMAND bash "-c" "mkdir -p ${THIRD_PARTY_DIR}/grpc_plugin"
-        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build_grpc_plugin
+        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/${BUILD_DIRECTORY}
         RESULT_VARIABLE grpc_plugin_mkdir_result
         OUTPUT_VARIABLE grpc_plugin_mkdir_out)
 MESSAGE(STATUS "grpc_plugin_mkdir_ERROR:" ${grpc_plugin_mkdir_result})
@@ -51,9 +53,9 @@ set(grpc_plugin_SOURCE_DIR ${THIRD_PARTY_DIR}/grpc_plugin)
 
 # create grpc_cpp_plugin copy to thirparty dir
 execute_process(
-        COMMAND bash "-c" "cp ${grpc_SOURCE_DIR}/build/grpc_cpp_plugin ${THIRD_PARTY_DIR}/grpc_plugin/"
-        COMMAND bash "-c" "cp ${grpc_SOURCE_DIR}/build/libgrpc_plugin_support.a ${THIRD_PARTY_DIR}/grpc_plugin/"
-        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/build_grpc_plugin
+        COMMAND bash "-c" "cp ${grpc_SOURCE_DIR}/${BUILD_DIRECTORY}/grpc_cpp_plugin ${THIRD_PARTY_DIR}/grpc_plugin/"
+        COMMAND bash "-c" "cp ${grpc_SOURCE_DIR}/${BUILD_DIRECTORY}/libgrpc_plugin_support.a ${THIRD_PARTY_DIR}/grpc_plugin/"
+        WORKING_DIRECTORY ${grpc_SOURCE_DIR}/${BUILD_DIRECTORY}
         RESULT_VARIABLE grpc_plugin_install_result
         OUTPUT_VARIABLE grpc_plugin_install_out)
 MESSAGE(STATUS "grpc_plugin_install_ERROR:" ${grpc_plugin_install_result})
