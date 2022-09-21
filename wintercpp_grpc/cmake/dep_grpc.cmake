73bd442a6f080bd7dadec23be54a225c3737d48e
samuaz <samuaz@gmail.com>
2022-09-21 02:03:27 -0300

    remove re2

--- wintercpp_grpc/cmake/dep_grpc.cmake
+++ wintercpp_grpc/cmake/dep_grpc.cmake
@@ -36,7 +36,7 @@ set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE INTERNAL "")
 set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE INTERNAL "")
 set(gRPC_SSL_PROVIDER package CACHE INTERNAL "")
 
-FetchContent_MakeAvailable(re2 grpc)
+FetchContent_MakeAvailable(grpc)
 
 # FetchContent_GetProperties(re2)
 # if(NOT re2_POPULATED)
