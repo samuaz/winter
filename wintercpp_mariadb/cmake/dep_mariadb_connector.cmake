ea1e4331eb46281b5ea30f2ecebdfbf296441875
samuaz <samuaz@gmail.com>
2022-09-21 15:54:51 -0300

    remove mariadb tests

--- wintercpp_mariadb/cmake/dep_mariadb_connector.cmake
+++ wintercpp_mariadb/cmake/dep_mariadb_connector.cmake
@@ -65,6 +65,14 @@ MESSAGE(STATUS "mariadb_c_patches_CMD_ERROR:" ${mariadb_c_patches_result})
 MESSAGE(STATUS "mariadb_c_patches_CMD_OUTPUT:" ${mariadb_c_patches_VARIABLE})
 
 execute_process(
+        COMMAND bash "-c" "rm -rf test"
+        WORKING_DIRECTORY ${mariadb_connector_SOURCE_DIR}
+        RESULT_VARIABLE mariadb_remove_test_result
+        OUTPUT_VARIABLE mariadb_remove_test_VARIABLE)
+MESSAGE(STATUS "mariadb_remove_test_CMD_ERROR:" ${mariadb_remove_test_result})
+MESSAGE(STATUS "mariadb_remove_test_CMD_OUTPUT:" ${mariadb_remove_test_VARIABLE})
+
+execute_process(
         COMMAND bash "-c" "cmake -DBUILD_SHARED_LIBS=OFF -DMARIADB_LINK_DYNAMIC=OFF -DINSTALL_LIB_SUFFIX=${mariadb_connector_SOURCE_DIR}/install -DCMAKE_INSTALL_PREFIX=${mariadb_connector_SOURCE_DIR}/install && make install"
         WORKING_DIRECTORY ${mariadb_connector_SOURCE_DIR}
         RESULT_VARIABLE mariadb_cmake_result
