b900d038efda13354a8efe038b03e917fdcc3d83
samuaz <samuaz@gmail.com>
2022-10-15 03:37:16 -0300

    fix cpp_redis

--- wintercpp_redis/cmake/dep_redis.cmake
+++ wintercpp_redis/cmake/dep_redis.cmake
@@ -5,14 +5,6 @@
 include(${PARENT_DIR}/cmake//host_utils.cmake)
 set(THIRD_PARTY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party)
 
-FetchContent_Declare(
-        tacopie
-        GIT_REPOSITORY https://github.com/cpp-redis/tacopie.git
-        GIT_TAG        master
-        GIT_PROGRESS   TRUE
-        SOURCE_DIR ${THIRD_PARTY_DIR}/tacopie
-)
-
 ## cpp_redis
 FetchContent_Declare(
         cpp_redis
@@ -21,8 +13,6 @@ FetchContent_Declare(
         GIT_PROGRESS   TRUE
         SOURCE_DIR ${THIRD_PARTY_DIR}/cpp_redis
 )
-
-
 ## END cpp_redis
 
 # FetchContent_GetProperties(cpp_redis)
@@ -31,7 +21,7 @@ FetchContent_Declare(
 #     add_subdirectory(${cpp_redis_SOURCE_DIR} ${cpp_redis_BINARY_DIR} EXCLUDE_FROM_ALL)
 # endif()
 
-FetchContent_MakeAvailable(tacopie cpp_redis)
-include_directories(${tacopie_SOURCE_DIR}/includes)
+FetchContent_MakeAvailable(cpp_redis)
+include_directories(${cpp_redis_SOURCE_DIR}/tacopie/includes)
 include_directories(${cpp_redis_SOURCE_DIR}/includes)
-set(WINTER_REDIS_LIB tacopie cpp_redis )
+set(WINTER_REDIS_LIB tacopie cpp_redis)
