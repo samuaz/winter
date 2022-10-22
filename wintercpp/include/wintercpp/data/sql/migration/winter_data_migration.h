d722ec1c967067026a3fca078d266c4cef63feb8
samuaz <samuaz@gmail.com>
2022-10-22 16:59:26 -0300

    update

--- wintercpp/include/wintercpp/data/sql/migration/winter_data_migration.h
+++ wintercpp/include/wintercpp/data/sql/migration/winter_data_migration.h
@@ -55,7 +55,7 @@ namespace winter::data::sql_impl {
             const unsigned char *str = reinterpret_cast<unsigned char*>(const_cast<char*>(script.c_str()));
             unsigned char hash[SHA_DIGEST_LENGTH]; // == 20
             SHA1(str, sizeof(str) - 1, hash);
-            std::cout << "el sha es: " << reinterpret_cast<char*>(hash) << std::endl;
+            std::cout << "el sha es: " << std::string(reinterpret_cast<char*>(hash)) << std::endl;
             // return h1 ^ (h2 << 1);
             return std::hash<std::string>()(script);
             ;
